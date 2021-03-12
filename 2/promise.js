453ee4db18661d15bfc7b5b194785f644189a23d
Nathan <nathancai1031@gmail.com>
2021-03-12 16:04:38 -0500

    Fixed bug

--- 2/promise.js
+++ 2/promise.js
@@ -5,3 +5,4 @@ const myPromise = new Promise((resolve, reject) => {
 myPromise
     .then(val => console.log(val))
     .catch(val => console.error(val))
+    .finally(() => console.log("this is the finally!"))
